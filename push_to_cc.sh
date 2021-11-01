0980fc58e93a0c728f19569210003a77120fb482
Austin Welch <austinmw@users.noreply.github.com>
2021-11-01 12:57:49 -0400

    scripts

---
+++ push_to_cc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git push ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/ByteTrackCC --all
---
+++ scripts/run_docker.sh
@@ -0,0 +1,20 @@
+DEFAULT_IMAGE=197237853439.dkr.ecr.us-west-2.amazonaws.com/bytetrack/training
+DOCKER_IMAGE="${1:-$DEFAULT_IMAGE}"
+
+echo $DISPLAY
+xauth list
+
+# cd ~/ByteTrack
+
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && \
+docker run --gpus all -it \
+-v $PWD:/workspace/ByteTrack \
+-v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
+--device /dev/video0:/dev/video0:mwr \
+--net=host \
+--ipc=host \
+-e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+-e DISPLAY=$DISPLAY \
+--privileged \
+--name byte \
+$DOCKER_IMAGE
